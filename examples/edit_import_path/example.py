447a1079f9eccc8c70f1e4d9ae20f08cc11648f2
Matthew Feickert <matthew.feickert@cern.ch>
2023-09-06 20:18:22 -0500

    Add jupytext

--- examples/edit_import_path/example.py
+++ examples/edit_import_path/example.py
@@ -1,3 +1,4 @@
+# %%
 # https://docs.scipy.org/doc/scipy-1.11.2/tutorial/optimize.html
 import sys
 from pathlib import Path
@@ -5,10 +6,14 @@ from pathlib import Path
 import numpy as np
 from scipy.optimize import minimize
 
+# %%
 # Make ./code/utils.py visible to sys.path
-sys.path.insert(1, str(Path(__file__).parent / "code"))
+sys.path.insert(1, str(Path().cwd() / "code"))
+# If file is a .py file and not a notebook, then can use:
+# sys.path.insert(1, str(Path(__file__).parent / "code"))
 from utils import rosen, rosen_der
 
+# %%
 x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
 result = minimize(rosen, x0, method="BFGS", jac=rosen_der, options={"disp": True})
 optimized_params = result.x
