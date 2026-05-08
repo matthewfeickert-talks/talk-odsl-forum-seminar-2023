31849c2c361e64384ce6e183195567ae353fd623
Matthew Feickert <matthew.feickert@cern.ch>
2026-05-08 12:51:18 -0600

    bring git back

--- docker_build.sh
+++ docker_build.sh
@@ -6,12 +6,12 @@ docker pull ghcr.io/prefix-dev/pixi:noble
 
 docker system prune -f
 
-# docker build \
-#     --file ./examples/pixi_build/Dockerfile \
-#     --tag talk-odsl-forum-seminar/pixi-build:debug \
-#     .
-
 docker build \
-    --file ./examples/pixi_build/uv_Dockerfile \
-    --tag talk-odsl-forum-seminar/pixi-build:uv-debug \
+    --file ./examples/pixi_build/Dockerfile \
+    --tag talk-odsl-forum-seminar/pixi-build:debug \
     .
+
+# docker build \
+#     --file ./examples/pixi_build/uv_Dockerfile \
+#     --tag talk-odsl-forum-seminar/pixi-build:uv-debug \
+#     .
